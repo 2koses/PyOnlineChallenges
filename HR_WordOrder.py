81b93d3868c21b457e071f1ccba2ca8a48bfa7b7
2koses <43809330+2koses@users.noreply.github.com>
2019-07-02 18:41:34 +0530

    Create HR_WordOrder.py

---
+++ HR_WordOrder.py
@@ -0,0 +1,11 @@
+from collections import Counter, OrderedDict
+l=OrderedDict()
+for i in range(int(input())):
+    x=input()
+    if x not in l:
+        l[x]=1
+    else:
+        l[x]=l[x]+1
+print(len(l))    
+for i in l:
+    print(l[i],end=" ") 
